f9abdad45359a4ccd7a478a44728dc1d9b86d835
MarIsa-17 <nunezlopezmarisa90@gmail.com>
2025-07-05 15:07:41 -0500

    Subida Inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+let nombre = prompt( "Escriba su nombre");
+let edad = prompt ("Escriba su edad");
+let ciudad= prompt("¿En que ciudad vives?")
+
+alert (`Hola ${nombre} 👋 , tienes ${edad} años y vives en ${ciudad}. !Bienvenid@!`);
+console.log(`Nombre Ingresado: ${nombre}`);
+console.log(`Edad Ingresada: ${edad}`);
+console.log(`Ciudad Ingresada: ${ciudad}`);
+
+alert("¡Bienvenido! Eres menor de edad 🧒");
+alert("¡Bienvenido adulto independiente 🎉!");
